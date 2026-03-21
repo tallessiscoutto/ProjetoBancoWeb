b676c7d69a1c430509bb207b610b76e15c8a9cc2
tallessiscoutto <tallessiscoutto09@gmail.com>
2026-03-21 19:59:07 -0300

    tentando arrumar tabela vendas

--- database/migrations/2025_04_09_232216_create_vendas_table.php
+++ database/migrations/2025_04_09_232216_create_vendas_table.php
@@ -7,16 +7,17 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up()
-    {
+{
+    if (!Schema::hasTable('vendas')) {
         Schema::create('vendas', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('produto_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('produto_id')->constrained('produtos');
             $table->integer('quantidade');
             $table->decimal('preco_total', 10, 2);
             $table->timestamps();
         });
-        
     }
+}
     public function down(): void
     {
         Schema::dropIfExists('vendas');
